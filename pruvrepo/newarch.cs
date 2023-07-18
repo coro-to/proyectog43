11882f97c3696ee20b8429b606f94db13e89d8cf
omar <ocoronadotolosa@gmail.com>
2023-07-17 20:16:35 -0500

    este es otro archivo de texto

---
+++ pruvrepo/newarch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruvrepo
+{
+    public class newarch
+    {
+        public void othermsj() 
+        {
+            Console.WriteLine("este es otro mensaje");
+        
+        }
+    }
+}
